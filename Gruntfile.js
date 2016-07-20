d3fec64bf86973d579492a6399e35c4547bbe89a
Hung Quach <quachio@icloud.com>
2016-07-20 11:25:39 -0700

    add sass feature

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,52 @@
+
 module.exports = function(grunt) {
 
   // Grunt Project Configurations
   grunt.initConfig({
     // Pass options to grunt configuration
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
+    pkg: grunt.file.readJSON('package.json'), // PGK plugin
+    concat: { // Concat Plugin
+      options: { // Name of the Concat task
         stripBanners: true,
         banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %> */',
       },
-      js: { // js job/task
+      js: { // Name of Js job/task
         src:  ['js/*.js'],
         dest: 'build/scripts.js',
       },
-      css: { // css job/task
-        src:  ['css/*.css'],
+      css: { // Name of Css job/task
+        // -> does not care bout the order src:  ['css/*.css'],
+        src:  ['css/reset.css', 'css//bootstrap.css', 'css/style.css'],
         dest: 'build/style.css',
-      }
+      },
     },
     log: {
       foo: [1, 2, 3],
       bar: 'hellow world',
       baz: false,
     },
+    sass: {
+      build: {
+        files: [{
+          src: 'css/sass/styles.scss',
+          dest: 'css/style.css',
+        },],
+      },
+    },
   });
 
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-sass');
 
   // Register tasks
-  /*
+
+  // Removed: Use this if you want to use only of the task
   grunt.registerTask('concat-js',  ['concat:js']);
   grunt.registerTask('concat-css', ['concat:css']);
-  */
+  grunt.registerTask('build-css', ['sass:build']);
+
 
   grunt.registerTask('run', function() {
     console.log('I am running');
